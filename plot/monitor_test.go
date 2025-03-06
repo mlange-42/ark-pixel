b7ca00566feb1eda639c37ba8a04466cca03f556
mlange-42 <martin_lange_@gmx.net>
2025-03-06 23:32:02 +0100

    fix tests

--- plot/monitor_test.go
+++ plot/monitor_test.go
@@ -23,13 +23,11 @@ func ExampleMonitor() {
 		Steps: 100,
 	})
 
-	//app.Run()
-
 	// Run the simulation.
 	// Due to the use of the OpenGL UI system, the model must be run via [window.Run].
 	// Uncomment the next line. It is commented out as the CI has no display device to test the model run.
 
-	window.Run(app)
+	//window.Run(app)
 
 	// Output:
 }
