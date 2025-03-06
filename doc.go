780fdc8ef1797073c7f5211ff6401c023bbe162d
mlange-42 <martin_lange_@gmx.net>
2025-03-06 22:17:47 +0100

    License, README, gitignore

---
+++ doc.go
@@ -0,0 +1,6 @@
+// Package archepixel provides OpenGL graphics and gonum plots for the Ark ECS using the Pixel game engine.
+//
+// See the sub-packages:
+//   - Live plotting and other visualizations -- [github.com/mlange-42/ark-pixel/plot]
+//   - OpenGL window system, for hosting visualizations -- [github.com/mlange-42/ark-pixel/window]
+package archepixel
