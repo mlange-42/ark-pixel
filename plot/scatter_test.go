bf9d7e06da7a63182d5bef98eb9108bbb82121fb
mlange-42 <martin_lange_@gmx.net>
2025-03-07 14:00:13 +0100

    fix tests

--- plot/scatter_test.go
+++ plot/scatter_test.go
@@ -38,13 +38,11 @@ func ExampleScatter() {
 		Steps: 100,
 	})
 
-	app.Run()
-
 	// Run the simulation.
 	// Due to the use of the OpenGL UI system, the model must be run via [window.Run].
-	// Comment out the code line above, and uncomment the next line to run this example stand-alone.
+	// Uncomment the next line to run this example stand-alone.
 
-	// window.Run(app)
+	//window.Run(app)
 
 	// Output:
 }
